40f70af4f4743cd0841049f711edbfd3b488b006
marianorigo18 <marianowagnerrigo@gmail.com>
2023-01-14 12:06:21 -0300

    first commit

--- js/nav.js
+++ js/nav.js
@@ -3,6 +3,15 @@ const navIcons = document.getElementById("nav__icons");
 const navClose = document.getElementById("nav__close");
 const navMenu = document.getElementById("nav__menu");
 const navbar = document.getElementById("nav__navbar");
+const navItem = document.querySelectorAll(".nav__item");
+
+navItem.forEach(element => {
+    element.addEventListener("click", ()=>{
+        navMenu.classList.toggle("nav__close")
+        navClose.classList.toggle("icon__visivility")
+        navbar.classList.toggle("menu__visivility")
+    })
+});
 
 navIcons.addEventListener("click", ()=> {
     navMenu.classList.toggle("nav__close")
